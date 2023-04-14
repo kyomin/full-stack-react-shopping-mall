dc9a566886e5f5119e3a4071093ec2ceeeddcd2b
jake.kyo <jake.kyo@kakaocorp.com>
2023-04-14 16:42:03 +0900

    장바구니에 recoil 상태관리 라이브러리 적용

--- src/components/product/item.tsx
+++ src/components/product/item.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import { Product } from '../../graphql/products';
+import { cartItemSelector } from '../../recoils/cart';
+import { useRecoilState } from 'recoil';
 
 const ProductItem = ({
   id,
@@ -8,14 +10,23 @@ const ProductItem = ({
   imageUrl,
   description,
   createdAt,
-}: Product) => (
-  <li className='product-item'>
-    <Link to={`/products/${id}`}>
-      <p className='product-item-title'>{title}</p>
-      <img className='product-item-image' src={imageUrl} />
-      <p className='product-item-price'>${price}</p>
-    </Link>
-  </li>
-);
+}: Product) => {
+  const [cartAmount, setCartAmount] = useRecoilState(cartItemSelector(id));
+  const addToCart = () => setCartAmount((prev) => (prev || 0) + 1);
+
+  return (
+    <li className='product-item'>
+      <Link to={`/products/${id}`}>
+        <p className='product-item-title'>{title}</p>
+        <img className='product-item-image' src={imageUrl} />
+        <p className='product-item-price'>${price}</p>
+      </Link>
+      <button className='product-item-add-cart' onClick={addToCart}>
+        담기
+      </button>
+      <p>{cartAmount || 0}</p>
+    </li>
+  );
+};
 
 export default ProductItem;
