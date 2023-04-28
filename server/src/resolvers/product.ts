f272afe330b8765651279d4fe1973685f25568e6
jake.kyo <jake.kyo@kakaocorp.com>
2023-04-28 22:48:43 +0900

    서버 상품 목록 쿼리 페이지네이션 적용

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -2,8 +2,12 @@ import { Resolver } from './types';
 
 const productResolver: Resolver = {
   Query: {
-    products: (parent, args, { db }, info) => {
-      return db.products;
+    products: (parent, { cursor = '' }, { db }, info) => {
+      const limit = 15; // 15개씩 끊겠다.
+      const fromIndex =
+        db.products.findIndex((product) => product.id === cursor) + 1;
+
+      return db.products.slice(fromIndex, fromIndex + limit) || [];
     },
     product: (parent, { id }, { db }, info) => {
       const found = db.products.find((item) => item.id === id);
